9318cd9f9e640eada81050b9c1882008f2895a78
Madison Hutchings <maddiehutch20@gmail.com>
2024-11-14 08:57:47 -0700

    Initial Commit

---
+++ RecipeApp2024/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  RecipeApp2024
+//
+//  Created by McKinsey Hutchings on 11/14/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
